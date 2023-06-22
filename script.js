0682b8bc2d8d894edf79ab95fba1e927ab7d7054
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-22 10:26:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function updateSizeInfo() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const sizeHeading = document.getElementById('sizeHeading');
+      sizeHeading.textContent = `Width: ${width}, Height: ${height}`;
+    }
+
+    // Update the size information when the window is resized
+    window.addEventListener('resize', updateSizeInfo);
+
+    // Call the updateSizeInfo() function initially to display the initial size
+    updateSizeInfo();
